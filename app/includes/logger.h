95dc9113377011896d469bd80613cfe02953ab21
bogdzn <wontsleep@protonmail.com>
2020-09-26 14:39:32 +0200

    Mod: setting comments to either Javadoc or C++ Doxygen compatible comments.

--- app/includes/logger.h
+++ app/includes/logger.h
@@ -10,34 +10,33 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-// path where the log file is stored.
+/// path where the log file is stored.
 char const *LOG_PATH = "/var/log/tello.log";
 
-// Macro opening the log file, with O_APPEND flag set.
+/// Macro opening the log file, with O_APPEND flag set.
 #ifndef OPEN_LOGFILE
 #define OPEN_LOGFILE (open(LOG_PATH, \
         O_NDELAY | O_CREAT | O_APPEND, O_RDWR, \
         S_IRUSR | S_IRGRP | S_IROTH))
 #endif
 
-// macro for truncating the log file.
+/// macro for truncating the log file.
 #ifndef CREATE_LOGFILE
 #define CREATE_LOGFILE (open(LOG_PATH, \
         O_NDELAY | O_CREAT | O_TRUNC, O_RDWR, \
         S_IRUSR | S_IRGRP | S_IROTH))
 #endif
 
-// enum defining the type of log we want to print.
+/// enum defining the type of log we want to print.
 typedef enum LOG_TYPE = {
     INFO, WARNING, ERROR
 };
 
 // CREATE_LOGFILE.C
-
-// creates the log file in /var/log/tello.log. Returns 0 if successful.
+/// creates the log file in /var/log/tello.log. Returns 0 if successful.
 int create_logfile(void);
 
-/*
+/*!
  * appends a specific message stored in /var/log/tello.log.
  * You will need to specify wether the LOG_TYPE (INFO, WARNING, ERROR),
  * and then you can just use it as a printf.
@@ -45,10 +44,10 @@ int create_logfile(void);
 void log(LOG_TYPE, char *string, ...);
 
 
-// Tests if logfile is present. For debug purposes.
+/// Tests if logfile is present. For debug purposes.
 bool logfile_exists(void);
 
-/*
+/*!
  * Sends a log to /var/log/tello.log, if errno is different that 2 or 0.
 */
 void log_if_errno(int errno, char *function_name);
