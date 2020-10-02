2c0037442c45f314c780a90025ab01fd6841cf99
bogdzn <wontsleep@protonmail.com>
2020-10-02 21:35:45 +0200

    Mod: if a socket fails to be created, program exits.

--- app/src/net/socket_handler.c
+++ app/src/net/socket_handler.c
@@ -10,28 +10,37 @@
 #include "utils.h"
 #include <errno.h>
 
+static void close_if_errno(int err, int sock, char *func_name)
+{
+    if (err != 2 && errno != 0) {
+        close(sock);
+        log_if_errno(errno, func_name);
+        _exit(1);
+    }
+}
+
 socket_t create_socket(char const *ip_addr, int port)
 {
     int sock_option = 1;
     socket_t result;
 
     __log(INFO, "creating socket for address %s:%i\n", ip_addr, port);
-    result.port = (port < 0) ? 7070 : port;
-    result.ip_addr = (!ip_addr) ? "127.0.0.1" : my_strdup(ip_addr);
+    result.port = (port < 0) ? RYZE_PORT : port;
+    result.ip_addr = (!ip_addr) ? RYZE_IP_ADDR : my_strdup(ip_addr);
     result.socket = socket(AF_INET, SOCK_STREAM, 0);
-    log_if_errno(errno, "create socket");
+    close_if_errno(errno, result.socket, "create socket");
 
     result.addr.sin_addr.s_addr = inet_addr(result.ip_addr);
     result.addr.sin_port = htons(result.port);
     result.addr.sin_family = AF_INET;
     result.status = connect(result.socket, (struct sockaddr *) &result.addr, sizeof(result.addr));
-    log_if_errno(errno, "connecting to socket");
+    close_if_errno(errno, result.socket, "connecting to socket");
 
     bind(result.socket, (struct sockaddr *) &result.addr, sizeof(result.addr));
-    log_if_errno(errno, "bind");
+    close_if_errno(errno, result.socket, "bind");
 
     setsockopt(result.socket, SOL_SOCKET, SO_REUSEADDR, &sock_option, sizeof(sock_option));
-    log_if_errno(errno, "setsockopt");
+    close_if_errno(errno, result.socket, "setsockopt");
 
     if (is_socket_ok(result))
         __log(INFO, "Connected.\n");
